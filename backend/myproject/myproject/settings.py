b9b37843cc45002fe6a2c754d40508bd3fb97dd7
Sudeepramasamy <sudeep9568@gmail.com>
2025-02-05 20:52:41 +0530

    Added Procfile for deployment

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -64,6 +64,12 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://weekendsports.netlify.app/",  # Replace with your Netlify domain
+]
+
+ALLOWED_HOSTS = ["myapp.onrender.com", "localhost"]
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
